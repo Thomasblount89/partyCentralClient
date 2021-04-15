3183e3841cce0096c968ed2acbe5f82c022baafb
Thomas Blount <Thomasblount89@gmail.com>
2021-04-14 22:55:25 -0400

    first attempt at connecting client

--- src/helper/environment.js
+++ src/helper/environment.js
@@ -5,8 +5,8 @@ switch (window.location.hostname) {
     case 'localhost' || '127.0.0.1':
         APIURL = "http://localhost:4001";
         break;
-    case 'limitless-thicket-03619.herokuapp.com':
-        APIURL = 'https://tlr-my-flix.herokuapp.com'
+    case 'partycentralclient.herokuapp.com':
+        APIURL = 'https://partycentralclient.herokuapp.com'
 }
 
 export default APIURL;
